9ffff36fb1cf3f79b994a322a2cdceaf84f7a2a4
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2024-09-07 12:51:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function bt1() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0"
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0"
+
+}
+
+function bt2() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97"
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97"
+}
+
+function bt3() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2"
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2"
+}
+
+function bt4() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9"
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9"
+}
